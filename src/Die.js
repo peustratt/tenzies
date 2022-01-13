cf227b3382fbf71c65052f7846ca87ef24565a20
Peu <peustratt@gmail.com>
2022-01-12 23:49:36 -0300

    adicinado imagem de dado dinâmica

--- src/Die.js
+++ src/Die.js
@@ -1,28 +1,33 @@
 import React from "react"
 
-const diceSyle = [
-{
-    
-}
+const dicePattern = [
+    [2],
+    [3, 6],
+    [1, 2, 7],
+    [1, 3, 6, 7],
+    [1, 2, 3, 6, 7],
+    [1, 3, 4, 5, 6, 7]
 ]
 
 export default function Die(props) {
     const styles = {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
+
+
+
+    const dotsEl = [1,2,3,4,5,6,7].map((dot) => {
+        let indexDado = props.value - 1
+
+        return <span style={dicePattern[indexDado].includes(dot) ? {backgroundColor: "black"} : {backgroundColor: "transparent"}}></span>
+    })
     return (
-        <div 
-            className="die-face" 
+        <div
+            className="die-face"
             style={styles}
             onClick={props.holdDice}
         >
-           <span></span>
-           <span></span>
-           <span></span>
-           <span></span>
-           <span></span>
-           <span></span>
-           <span></span>
+            {dotsEl}
         </div>
     )
 }
